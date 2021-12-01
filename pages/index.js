ffbf8ed3bcce19cc736282367cc60515a8549f30
Nelson <franciscosilvanelson@gmail.com>
2021-11-30 22:48:39 -0300

    add anchor link with animation

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,28 @@
+import { useEffect } from 'react';
 import { Footer } from '../components/Footer';
 import Header from '../components/Header';
 import Services from '../components/Services';
 
-export default function Home() {
+export default function Index() {
+  useEffect(() => {
+    const element = document.getElementById(location.pathname);
+
+    window.scroll({
+      behavior: 'smooth',
+      top: element ? element.offsetTop : 0,
+    });
+  });
+
   return (
     <div>
       <Header />
       <Services />
+      <div id="/sobre" style={{ backgroundColor: 'white', height: 500 }}>
+        <h1 style={{ margin: 0 }}>Sobre</h1>
+      </div>
+      <div id="/contato" style={{ backgroundColor: 'white', height: 500 }}>
+        <h1 style={{ margin: 0 }}>Contato</h1>
+      </div>
       <Footer />
     </div>
   )
