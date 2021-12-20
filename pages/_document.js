d43fb5d9b591a318a042609acd6aa824ed32ef07
Nelson <nelson.silva@toroinvestimentos.com.br>
2021-12-20 11:58:16 -0300

    add viewport tag

--- pages/_document.js
+++ pages/_document.js
@@ -34,6 +34,7 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <meta charset="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=1.0, minimum-scale=1.0, maximum-scale=1.0" />
           <link rel="shortcut icon" href="favicon.ico" />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
           <meta name="description" content="Somos experts no desenvolvimento de sites e aplicativos para internet. Entendemos as necessidades do negócio e trabalhamos para construir soluções digitais com agilidade, flexibilidade e precisão." />
