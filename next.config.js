31a92a8090878c85b49a57d563996a5d58c3c4b3
Nelson <franciscosilvanelson@gmail.com>
2022-01-30 10:18:16 -0300

    ci: remove unused debug const

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@
 // So, we write it in vanilla JS
 // (But you could use ES2015 features supported by your Node.js version)
 
-const debug = process.env.NODE_ENV !== 'production';
-
 module.exports = {
   assetPrefix: '',
   images: {
